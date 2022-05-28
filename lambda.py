7f837f7cf6d0f27807b417f958c82e44950119fc
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-28 16:28:10 +0300

    Add current state of lambda function.

--- lambda.py
+++ lambda.py
@@ -1,81 +1,56 @@
-# import json
+"""The lambda function used."""
 
-# print('Loading function')
-
-
-# def lambda_handler(event, context):
-#     # 1. Parse out query string params
-#     transactionId = event['queryStringParameters']['transactionId']
-#     transactionType = event['queryStringParameters']['type']
-#     transactionAmount = event['queryStringParameters']['amount']
-
-#     print(f'{transactionId=}')
-#     print(f'{transactionType=}')
-#     print(f'{transactionAmount=}')
-
-#     # 2. Construct the body of the response object
-#     response = {
-#         'transactionId': transactionId,
-#         'type': transactionType,
-#         'amount': transactionAmount,
-#         'message': 'Hello from Lambda land!'
-#     }
-
-#     # 3. Construct http response object
-#     responseObject = {
-#         'statusCode': 200,
-#         'headers': {
-#             'Content-Type': 'application/json'
-#         },
-#         'body': json.dumps(response)
-#     }
-
-#     # 4. Return the response object
-#     return responseObject
-
+import json
+import urllib.error
+import urllib.parse
+import urllib.request
 
 import boto3
-from decimal import Decimal
-import json
-import urllib.request
-import urllib.parse
-import urllib.error
 
 print('Loading function')
 
+RES_BUCKET = 'viztranz-results'
+
+s3 = boto3.client('s3')
 rekognition = boto3.client('rekognition')
 
 
 # --------------- Helper Functions to call Rekognition APIs ------------------
 
 def detect_labels(bucket, key):
-    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    """Call rekognition DetectLabels API to detect labels in S3 object."""
+    response = rekognition.detect_labels(
+        Image={"S3Object": {"Bucket": bucket, "Name": key}}
+    )
 
-    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
-    # Note: role used for executing this Lambda function should have write access to the table.
-    #table = boto3.resource('dynamodb').Table('MyTable')
-    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
-    #table.put_item(Item={'PK': key, 'Labels': labels})
-    return response
+    to_translate = {d['Name']: d['Confidence'] for d in response['Labels']}
+
+    return to_translate
+
+
+def upl_to_s3(bucket, res, key):
+    """Upload the translations along with the confidence levels to S3."""
+    to_upload_bytes = bytes(json.dumps(res).encode('UTF-8'))
+    s3.put_object(Bucket=bucket, Key=key, Body=to_upload_bytes)
+
 
 # --------------- Main handler ------------------
 
-
 def lambda_handler(event, context):
-    '''Demonstrates S3 trigger that uses
-    Rekognition APIs to detect faces, labels and index faces in S3 Object.
-    '''
-    #print("Received event: " + json.dumps(event, indent=2))
-
+    """Parse the S3 image, call Rekognition API and Translate API, and return result."""
     # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+
     try:
-        # Calls rekognition DetectLabels API to detect labels in S3 object
-        response = detect_labels(bucket, key)
+        to_translate = detect_labels(bucket, key)
 
         # Print response to console.
-        print(response)
+        print(to_translate)
 
-        return response
+        upl_to_s3(RES_BUCKET, to_translate, key)
+
+        return to_translate
     except Exception as e:
         print(e)
         print("Error processing object {} from bucket {}. ".format(key, bucket) +
