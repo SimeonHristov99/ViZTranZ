bf17958b8eaa6d7abc5027691e0538e79e4d8ead
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-28 16:28:02 +0300

    Prettify code.

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
+"""Main application."""
+
 import io
+
 import streamlit as st
 from PIL import Image
-from s3_uploader import upload
 
+from s3_uploader import upload
 
 st.set_page_config(page_icon='📷', page_title='ViZTranZ')
 
 
 def show_welcome():
+    """Display the welcome message and description of application."""
     st.title(f'📷 ➡ 📖')
     st.title('ViZTranZ')
     st.header('A Visual Translation Service')
 
 
 def get_upl_file():
+    """Show menu to upload image and return uploaded image."""
     st.subheader('Step 1: Upload an image')
 
     result = st.file_uploader(
@@ -34,6 +39,7 @@ def get_upl_file():
 
 
 def get_langs():
+    """Show menu for choosing a language and return selection."""
     st.subheader('Step 2: Choose language(s) to translate to')
 
     options = st.multiselect(
@@ -46,6 +52,7 @@ def get_langs():
 
 
 def main():
+    """Handle process of using the application."""
     show_welcome()
 
     in_mem_file = get_upl_file()
@@ -60,7 +67,8 @@ def main():
 
             if st.button('Translate!', help='Click this button to begin the \
                 translation process'):
-                upload(in_mem_file)
+                if upload(in_mem_file):
+                    st.write('Successful upload!')
 
 
 if __name__ == '__main__':
--- s3_uploader.py
+++ s3_uploader.py
@@ -1,9 +1,10 @@
+"""Code to upload file to S3."""
+
 import os
-import boto3
-
-import string
 import secrets
+import string
 
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,26 +13,30 @@ SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
 
 
 def gen_name(name_len=16):
+    """Generate random name including characters and digits."""
     alphabet = string.ascii_letters + string.digits
     name = ''.join(secrets.choice(alphabet) for _ in range(name_len))
     return name
 
 
 def upload(in_mem_file):
+    """Upload the file to S3."""
     bucket_name = 'viztranz'
     file_key = f'im_{gen_name()}'
 
-    client = boto3.client('s3',
-                          aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_ACCESS_KEY,
-                          )
-
-    client.upload_fileobj(in_mem_file, bucket_name, file_key)
-    print('upload successful??')
-
+    try:
+        client = boto3.client('s3',
+                            aws_access_key_id=ACCESS_KEY,
+                            aws_secret_access_key=SECRET_ACCESS_KEY,
+                            )
+        client.upload_fileobj(in_mem_file, bucket_name, file_key)
+        return True
+    except:
+        return False
 
 def main():
-    print('Hello, world!')
+    """Print a hello message from the current file."""
+    print(f'Hello, from {__name__}')
 
 
 if __name__ == '__main__':
