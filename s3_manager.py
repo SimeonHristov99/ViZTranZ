9f5598b25f4fbe9a62708f9cb79f22021de406b3
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-28 17:35:04 +0300

    Rename and add a function to download results.

--- s3_manager.py
+++ s3_manager.py
@@ -7,9 +7,19 @@ import string
 import boto3
 from dotenv import load_dotenv
 
+from io import BytesIO
+from time import sleep
+
 load_dotenv()
+
 ACCESS_KEY = os.getenv('ACCESS_KEY')
 SECRET_ACCESS_KEY = os.getenv('SECRET_ACCESS_KEY')
+BUCKET_RES = os.getenv('BUCKET_RES')
+
+client = boto3.client('s3',
+                      aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_ACCESS_KEY,
+                      )
 
 
 def gen_name(name_len=16):
@@ -25,14 +35,28 @@ def upload(in_mem_file):
     file_key = f'im_{gen_name()}'
 
     try:
-        client = boto3.client('s3',
-                            aws_access_key_id=ACCESS_KEY,
-                            aws_secret_access_key=SECRET_ACCESS_KEY,
-                            )
         client.upload_fileobj(in_mem_file, bucket_name, file_key)
-        return True
+        return file_key
     except:
-        return False
+        return None
+
+
+def get_results(file_name):
+    """Return the results from the translation."""
+    results = []
+    while 'Contents' not in results:
+        results = client.list_objects_v2(
+            Bucket=BUCKET_RES,
+            Prefix=file_name,
+            MaxKeys=1
+        )
+        sleep(3)
+
+    data = client.get_object(Bucket=BUCKET_RES, Key=file_name)
+    contents = data['Body'].read()
+    result = contents.decode("utf-8")
+    return dict(eval(result))
+
 
 def main():
     """Print a hello message from the current file."""
