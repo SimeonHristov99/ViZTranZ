9cabbaa5593501e486185a91ce78ade27693af16
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-29 18:05:13 +0300

    Move functions to another file.

--- app.py
+++ app.py
@@ -1,129 +1,60 @@
 """Main application."""
 
-import io
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import plotly.express as px
 import streamlit as st
-from PIL import Image
 
 import constants
+from helpers import build_chart, get_langs, get_upl_file, toggle_results
 from s3_manager import get_results, upload
 
-st.set_page_config(page_icon='📷', page_title='ViZTranZ')
+################################################################################
+# Title
+################################################################################
+
+st.set_page_config(
+    layout='wide',
+    page_icon='📷',
+    page_title='ViZTranZ'
+)
+
+st.title(f'📷 ➡ 📖')
+st.title('ViZTranZ')
+st.header('A Visual Translation Service in Two Simple Steps!')
 
 
-def toggle_results():
-    """Delete stored results to signal new image upload."""
-    constants.results = None
+################################################################################
+# Sidebar
+################################################################################
+
+option = st.sidebar.selectbox(
+    'Which mode would you like to use?',
+    ('Offline', 'AWS'),
+    on_change=toggle_results
+)
+constants.mode = option
+st.sidebar.write(f'Currently in mode `{option}`')
 
 
-def show_welcome():
-    """Display the welcome message and description of application."""
-    st.title(f'📷 ➡ 📖')
-    st.title('ViZTranZ')
-    st.header('A Visual Translation Service')
+################################################################################
+# Main menu
+################################################################################
 
+in_mem_file = get_upl_file()
 
-def get_upl_file():
-    """Show menu to upload image and return uploaded image."""
-    st.subheader('Step 1: Upload an image')
+if in_mem_file is not None:
+    st.image(in_mem_file, 'Your image')
 
-    result = st.file_uploader(
-        '',
-        help='Upload one picture by clicking the "Browse files" button\
-             or dragging the image.',
-        on_change=toggle_results
-    )
+    langs = get_langs()
 
-    if result is not None:
-        pil_image = Image.open(result)
+    if langs is not None and len(langs) > 0 \
+        and st.button(
+            'Translate!',
+            help='Click this button to begin the translation process'
+    ):
+        if constants.mode == 'AWS' and constants.results is None:
+            print(' ... uploading new image ... ')
+            file_name = upload(in_mem_file)
+            constants.results = get_results(file_name)
+        elif constants.mode == 'Offline':
+            constants.results = constants.get_sample_results()
 
-        # saving file to an in-memory file
-        in_mem_file = io.BytesIO()
-        pil_image.save(in_mem_file, format=pil_image.format)
-        in_mem_file.seek(0)
-
-        return in_mem_file
-
-
-def get_langs():
-    """Show menu for choosing a language and return selection."""
-    st.subheader('Step 2: Choose language(s) to translate to')
-
-    options = st.multiselect(
-        'Pick language(s) in which to translate',
-        constants.SUPPORTED_LANGS,
-        ['Bulgarian'],
-    )
-
-    return options
-
-
-def build_chart(langs):
-    """Create and display the bar chart with the confidence of the translations."""
-    if constants.results is None:
-        st.write('ERROR: No `results` for image found!')
-        return
-
-    if len(langs) < 1:
-        st.write('Please select at least one language it which to translate!')
-        return
-
-    res_df = pd.DataFrame(constants.results, index=[
-                          'Confidence'] + constants.SUPPORTED_LANGS).T
-    res_df['Confidence'] = res_df['Confidence'].astype(float)
-
-    for lang in langs:
-        st.subheader(f'Results for {lang}')
-
-        df = pd \
-            .concat([res_df[lang], res_df['Confidence']], axis=1) \
-            .nlargest(10, 'Confidence')
-
-        df[lang] = df.index + ' => ' + df[lang]
-
-        fig = px.bar(
-            df.iloc[::-1],
-            x='Confidence',
-            y=lang,
-            orientation='h',
-            height=800,
-        )
-
-        fig.update_layout(
-            font=dict(
-                size=16,
-            )
-        )
-
-        st.plotly_chart(fig, use_container_width=True)
-
-
-def main():
-    """Handle process of using the application."""
-    show_welcome()
-
-    in_mem_file = get_upl_file()
-
-    if in_mem_file is not None:
-        st.image(in_mem_file, 'Your image')
-
-        langs = get_langs()
-
-        if langs is not None and len(langs) > 0:
-            if st.button('Translate!', help='Click this button to begin the \
-                translation process'):
-                if constants.results is None:
-                    print(' ... uploading new image ... ')
-                    # constants.results = constants.get_res()
-                    file_name = upload(in_mem_file)
-                    constants.results = get_results(file_name)
-
-                build_chart(langs)
-
-
-if __name__ == '__main__':
-    main()
+        build_chart(langs)
---
+++ helpers.py
@@ -0,0 +1,93 @@
+"""Stores helper functions."""
+
+import io
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+
+import constants
+from s3_manager import get_results, upload
+
+
+def toggle_results():
+    """Delete stored results to signal new image upload."""
+    constants.results = None
+
+
+def get_upl_file():
+    """Show menu to upload image and return uploaded image."""
+    st.subheader('Step 1: Upload an image')
+
+    result = st.file_uploader(
+        '',
+        help='Upload one picture by clicking the "Browse files" button\
+             or dragging the image.',
+        on_change=toggle_results
+    )
+
+    if result is not None:
+        pil_image = Image.open(result)
+
+        # saving file to an in-memory file
+        in_mem_file = io.BytesIO()
+        pil_image.save(in_mem_file, format=pil_image.format)
+        in_mem_file.seek(0)
+
+        return in_mem_file
+
+
+def get_langs():
+    """Show menu for choosing a language and return selection."""
+    st.subheader('Step 2: Choose language(s) to translate to')
+
+    options = st.multiselect(
+        'Pick language(s) in which to translate',
+        constants.SUPPORTED_LANGS,
+        ['Bulgarian'],
+    )
+
+    return options
+
+
+def build_chart(langs):
+    """Create and display the bar chart with the confidence of the translations."""
+    if constants.results is None:
+        st.error('No translations for image found!')
+        return
+
+    if len(langs) < 1:
+        st.info('Please select at least one language it which to translate!')
+        return
+
+    res_df = pd.DataFrame(constants.results, index=[
+                          'Confidence'] + constants.SUPPORTED_LANGS).T
+    res_df['Confidence'] = res_df['Confidence'].astype(float)
+
+    for lang in langs:
+        st.subheader(f'Results for {lang}')
+
+        df = pd \
+            .concat([res_df[lang], res_df['Confidence']], axis=1) \
+            .nlargest(10, 'Confidence')
+
+        df[lang] = df.index + ' => ' + df[lang]
+
+        fig = px.bar(
+            df.iloc[::-1],
+            x='Confidence',
+            y=lang,
+            orientation='h',
+            height=800,
+        )
+
+        fig.update_layout(
+            font=dict(
+                size=16,
+            )
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
