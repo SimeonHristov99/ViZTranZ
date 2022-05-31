12e945472dfc2686ec3ea318bfd8a445209201ea
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-31 15:22:52 +0300

    Add return to main dir.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 echo 'Creating a new virtual environment ...'
 python3 -m venv venv
-source "./venv/bin/activate"
+source ./venv/bin/activate
 
 echo 'Installing application dependencies ...'
-pip3 install streamlit tensorflow torch matplotlib plotly boto3 python-dotenv transformers git-lfs
+pip3 install streamlit tensorflow torch matplotlib plotly boto3 python-dotenv transformers git-lfs sentencepiece tensorflow_hub sacremoses
 
 echo 'Setting up Tensorflow for performing object detection ...'
 mkdir tf_model
@@ -14,26 +14,26 @@ wget https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1?tf-hub-format=com
 mv 1\?tf-hub-format\=compressed model.gz
 gzip -d model.gz
 tar -xf model
-
-echo 'Setting up Pytorch and Hugging Face Transformers for neural machine translation ...'
-sudo apt-get install git-lfs
-git lfs install
+cd ..
 
 echo 'Downloading model for en-bg translations ...'
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/Helsinki-NLP/opus-mt-en-bg
 cd opus-mt-en-bg || exit
+git lfs install
 git lfs pull
 cd ..
 
 echo 'Downloading model for en-ru translations ...'
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/Helsinki-NLP/opus-mt-en-ru
 cd opus-mt-en-ru || exit
+git lfs install
 git lfs pull
 cd ..
 
 echo 'Downloading model for en-de translations ...'
 GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/Helsinki-NLP/opus-mt-en-de
 cd opus-mt-en-de || exit
+git lfs install
 git lfs pull
 cd ..
 
